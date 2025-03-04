218cca0ec9c3e2120caf0027deb073e3f2c1a893
Rahim Kaan Kacar <rahimkaanis@hotmail.com>
2025-03-04 18:39:07 +0300

    DbContext Oluşturuldu

--- BlogApp/Entity/Comment.cs
+++ BlogApp/Entity/Comment.cs
@@ -7,6 +7,13 @@ namespace BlogApp.Entity
 {
     public class Comment
     {
+        public int CommentId { get; set; }
+        public string? Text { get; set; }
+        public DateTime PublishedOn { get; set; }
         
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public Post Post { get; set; } = null!;
+        public User User { get; set; } = null!;
     }
 }
