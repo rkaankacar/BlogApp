caf9050fe82752c08b077a06ea32b520138ac93e
Rahim Kaan Kacar <rahimkaanis@hotmail.com>
2025-03-07 01:19:42 +0300

    ConnectionString oluşturuldu

--- BlogApp/obj/Debug/net9.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net9.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac8313552be119a7937847526d3a9783fd9ec6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218cca0ec9c3e2120caf0027deb073e3f2c1a893")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
